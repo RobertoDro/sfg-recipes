1b99ebde138f495095a97a5bf3aaa158b9946a6c
RobertoDro <37771467+RobertoDro@users.noreply.github.com>
2021-01-18 21:32:21 +0200

    create custom jpa method using property name

--- src/main/java/guru/springframework/sfgrecipes/controllers/IndexController.java
+++ src/main/java/guru/springframework/sfgrecipes/controllers/IndexController.java
@@ -1,15 +1,33 @@
 package guru.springframework.sfgrecipes.controllers;
 
+import guru.springframework.sfgrecipes.domain.Category;
+import guru.springframework.sfgrecipes.domain.UnitOfMeasure;
+import guru.springframework.sfgrecipes.repositories.CategoryRepository;
+import guru.springframework.sfgrecipes.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","/index"})
     public String getIndexPage()
     {
-        System.out.println("Some message to say...");
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id  is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
