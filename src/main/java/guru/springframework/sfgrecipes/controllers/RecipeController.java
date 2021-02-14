81264f6627f38f76eba28281a55bffb023557d49
RobertoDro <37771467+RobertoDro@users.noreply.github.com>
2021-02-14 12:57:30 +0200

    updating pom and imports for validation to work

--- src/main/java/guru/springframework/sfgrecipes/controllers/RecipeController.java
+++ src/main/java/guru/springframework/sfgrecipes/controllers/RecipeController.java
@@ -46,7 +46,7 @@ public class RecipeController {
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe")RecipeCommand command, BindingResult bindingResult){
 
         if(bindingResult.hasErrors()){
 
